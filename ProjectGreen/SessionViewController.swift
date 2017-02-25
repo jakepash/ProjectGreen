534ab2407afa0667c496e583af7a5dc168ec6261
adameliezerov@manzanitalabs.com <adaminspaceship@gmail.com>
2017-02-25 09:22:29 +0200

    Elements array working

--- ProjectGreen/SessionViewController.swift
+++ ProjectGreen/SessionViewController.swift
@@ -23,24 +23,28 @@ class SessionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    var myArray = ["0"]
+   
     
     @IBAction func JoinLobby(_ sender: Any) {
         
         ref = FIRDatabase.database().reference().child("Lobbies")
         //let key = String()
-        var myArray = ["0"]
+        
+        myArray.remove(at: 0)
+        
+        
         
         ref.observeSingleEvent(of: .value, with: { (FIRDataSnap) in
             for child in FIRDataSnap.children.allObjects {
                 let key = (child as AnyObject).key as String
-                myArray.append(key)
+                self.myArray.append(key)
             }
-            for (_, element) in myArray.enumerated() {
+            
+            
+            
+            
+            for (_, element) in self.myArray.enumerated() {
                 self.ref.child(element).child("Players").observeSingleEvent(of: .value, with: { (Snap) in
                     if Snap.childrenCount < 2 {
                         print ("Joining...")
@@ -58,16 +62,18 @@ class SessionViewController: UIViewController {
         })
         
         //...
-        
-        
-        
-        
-        
-        
-        
+
         
        
         
     }
+    @IBAction func PickerCheck(_ sender: Any) {
+        print (self.myArray.count)
+        
+        
+    }
 
+    @IBOutlet weak var PickArray: UIPickerView!
+
+    
 }
